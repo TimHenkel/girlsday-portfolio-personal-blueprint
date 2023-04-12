7649fdc10334dd49462d1e811d7c21005c3578d9
Matthias Hempel <m.hempel@me.com>
2023-04-12 21:03:00 +0200

    add more profile like content

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Card, Container, MantineProvider, Text, Title } from "@mantine/core";
+import { AiFillGithub, AiFillTwitterCircle, TiSocialLinkedinCircular }  from "react-icons/all"
+import { Container, Group, Image, MantineProvider, Stack, Text, Title } from "@mantine/core";
 
 function App() {
-  const [count, setCount] = useState(0);
 
+  
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Container>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <Title order={1}>Vite + React</Title>
-        <Card mt="xl">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <Text mt="sm">
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </Text>
-        </Card>
-        <Text mt="md" c="dimmed">
-          Click on the Vite and React logos to learn more
-        </Text>
+      <Container bg={"dark"} fluid={true} h={"100%"}>
+        <Group position="center">
+          <Stack h={300}>
+            <Title mt="xl" order={1} c="dimmed">
+              Matthias Hempel
+            </Title>
+            <Text mt="sm" c="#fff">
+              Senior Software Developer<br/> (aka. Swiss Army knife Developer) <br/> cyclist, father, husband, open-minded & considerate 
+            </Text>
+            <Group>
+              <AiFillGithub color="#fff"/>
+              <AiFillTwitterCircle color="#fff"/>
+              <TiSocialLinkedinCircular color="#fff"/>
+            </Group>
+          </Stack>
+          <Image src="/profile-image.jpeg" maw={240} radius="120px" />
+        </Group>
       </Container>
     </MantineProvider>
   );
