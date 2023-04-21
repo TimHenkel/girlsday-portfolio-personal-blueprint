08e103ee98401c29c8586c7181f8433042257c33
Matthias Hempel <m.hempel@me.com>
2023-04-21 21:14:18 +0200

    add Links for social icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import "./App.css";
 import { AiFillGithub, AiFillTwitterCircle, TiSocialLinkedinCircular }  from "react-icons/all"
-import { Container, Group, Image, MantineProvider, Stack, Text, Title } from "@mantine/core";
+import { ActionIcon, Container, Group, Image, MantineProvider, Stack, Text, Title } from "@mantine/core";
 
 function App() {
-
-  
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Container bg={"dark"} fluid={true} h={"100%"}>
@@ -17,9 +15,15 @@ function App() {
               Senior Software Developer<br/> (aka. Swiss Army knife Developer) <br/> cyclist, father, husband, open-minded & considerate 
             </Text>
             <Group>
-              <AiFillGithub color="#fff"/>
-              <AiFillTwitterCircle color="#fff"/>
-              <TiSocialLinkedinCircular color="#fff"/>
+              <ActionIcon variant="transparent" component="a" href="https://github.com/fotopixel" target="_blank">
+                <AiFillGithub href="https://github.com/fotopixel" color="#fff"/>
+              </ActionIcon> 
+              <ActionIcon variant="transparent" component="a" href="https://twitter.com/Fotopixel2010" target="_blank">
+                <AiFillTwitterCircle color="#fff"/>
+              </ActionIcon>
+              <ActionIcon variant="transparent" component="a" href="https://www.linkedin.com/in/matthias-hempel-62327678/" target="_blank">
+                <TiSocialLinkedinCircular color="#fff"/>
+              </ActionIcon>
             </Group>
           </Stack>
           <Image src="/girlsday-portfolio-blueprint/profile-image.jpeg" maw={240} radius="120px" />
