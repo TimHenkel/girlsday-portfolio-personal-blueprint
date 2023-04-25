1ed2164debd99af90499fffcb2b81304a6e68852
TimHenkel <131610046+TimHenkel@users.noreply.github.com>
2023-04-25 14:20:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/girlsday-portfolio-blueprint/",
+  base: "/girlsday-portfolio-personal-blueprint/",
   plugins: [react()],
 });
