9efe282f7193b8faa7775cdb380e0d6449af3084
tim henkel <timhenkel.privat@gmail.com>
2023-04-25 14:31:37 +0200

    Add first changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import "./App.css";
 import { AiFillGithub, AiFillTwitterCircle, TiSocialLinkedinCircular }  from "react-icons/all"
-import { ActionIcon, Container, Group, Image, MantineProvider, Stack, Text, Title } from "@mantine/core";
+import { ActionIcon, Avatar, Box, Container, Group, Image, MantineProvider, Space, Stack, Text, Title } from "@mantine/core";
 
 function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Container bg={"dark"} fluid={true} h={"100%"}>
+      <Space h={50} />
+        <Image src="/girlsday-portfolio-personal-blueprint/girlsday-pic.jpeg" height={250} fit="fill" mb={50}/>
+        <Container>
+
+        </Container>
+        <Container fluid={true} h={500} bg={"red"} mb={50}>
+          <Stack h={300} align="center" spacing="sm">
+            <Space h={50} />
+            <Group position="center" spacing={80}>
+              <Avatar component="a" href="https://www.nevermoreacademy.com/" target="_blank" src="/girlsday-portfolio-personal-blueprint/nevermore-logo.jpeg" size={200} radius={100}/>
+              <Avatar component="a" href="https://www.bitgrip.com/de/home" target="_blank" src="/girlsday-portfolio-personal-blueprint/bitgrip-logo.jpeg" size={200} radius={100}/>
+            </Group>
+            <Title mt="xl" order={1} weight={500} color="dark.6" align="center">
+              NEVERMORE ACADEMY <br/> & <br/> BITGRIP
+            </Title>
+          </Stack>
+        </Container>
         <Group position="center">
           <Stack h={300}>
-            <Title mt="xl" order={1} c="dimmed">
-              Matthias Hempel
-            </Title>
             <Text mt="sm" c="#fff">
               Senior Software Developer<br/> (aka. Swiss Army knife Developer) <br/> cyclist, father, husband, open-minded & considerate 
             </Text>
@@ -26,7 +40,6 @@ function App() {
               </ActionIcon>
             </Group>
           </Stack>
-          <Image src="/girlsday-portfolio-blueprint/profile-image.jpeg" maw={240} radius="120px" />
         </Group>
       </Container>
     </MantineProvider>
